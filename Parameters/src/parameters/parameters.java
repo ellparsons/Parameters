c3e9f4e4f9e33c713a9b87da000073acf1eafaa2
ellparsons <45197919+ellparsons@users.noreply.github.com>
2018-12-19 17:33:22 +0000

    Add files via upload

---
+++ Parameters/src/parameters/parameters.java
@@ -0,0 +1,13 @@
+package parameters;
+
+public class parameters {
+
+	public static void main(String[] args) {
+		Greeting ("Hello World!") ;
+
+	}
+
+	public static void Greeting (String a) {
+		System.out.println(a);
+	}
+}
